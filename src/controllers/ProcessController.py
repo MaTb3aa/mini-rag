5e3cb35f948aa0262dfc8413813e1b3cece43dcd
MaTb3aa <hossam.hakim.fci@gmail.com>
2025-05-18 18:41:02 +0300

    implement get_file_loadeer

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -1,6 +1,9 @@
 from .BaseController import BaseController
 from .ProjectController import ProjectController
 import os
+from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loader import PyMuPDFLoader
+from models import ProcessingEnum
 class ProcessController(BaseController):
     def __init__(self, project_id : str):
         super().__init__()
@@ -13,4 +16,16 @@ class ProcessController(BaseController):
         Check if the file exists in the project directory.
         """
         return os.path.splitext(file_id)[-1]
+
+    def get_file_loader(self, file_id: str, file_path : str):
+        """
+        Get the appropriate file loader based on the file extension.
+        """
+        file_extension = self.get_file_exists(self)
+        if file_extension == ProcessingEnum.TXT:
+            return TextLoader(file_path, encoding="utf-8")
+        elif file_extension == ProcessingEnum.PDF:
+            return PyMuPDFLoader(file_path, encoding="utf-8")
+        else:
+            raise ValueError(f"Unsupported file type: {file_extension}")
        
